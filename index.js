3a833ad8d6529d5aed67a2767b9be100edf5bdfd
iamvinxz <john.khervin13@gmail.com>
2025-10-25 18:51:32 +0800

    switch

--- index.js
+++ index.js
@@ -15,42 +15,58 @@ const server = http.createServer( async (req,res) => {
     const urlObj = new URL(req.url, `http://${req.headers.host}`)
     const queryObj = Object.fromEntries(urlObj.searchParams)
 
-    //using .pathname to set dynamic /api or /api with query parameters
-    if(urlObj.pathname === '/api' && req.method === 'GET'){
-
-        let filteredDestination = queryParams(queryObj, destinations)
-
-        //fetching data based on query parameters
-        ResponseJSON(res, 200, filteredDestination)
-        
-    }else if(req.url.startsWith('/api/continent') && req.method === 'GET'){
-
-        //extracting the last query param on url
-        const continent = req.url.split('/').pop()   
-        
-        //filtering data based on its locationType 
-        const filteredData = filtereddData(destinations, "continent", continent)
-       
-        ResponseJSON(res, 200, filteredData)
-
-    }else if(req.url.startsWith('/api/country') && req.method === 'GET'){
-
-        //extracting the last query param on url
-        const country = req.url.split('/').pop()
-
-        //filtering data based on its locationType 
-        const filteredData = filtereddData(destinations, "country",  country)
-
-        ResponseJSON(res, 200, filteredData)
-    }else{
-
-        res.statusCode = 404
-        res.setHeader("Content-Type", "application/json")
-        ResponseJSON(res, 404, ({
+    switch(req.method){
+        case "GET":
+            getRequest(req, res)
+            break
+        case "POST":
+            postRequest(req, res)
+            break
+        case "PUT":
+            putRequest(req, res)
+            break
+        case "DELETE":
+            deleteRequest(req, res)
+            break
+        default: 
+            res.setHeader("Content-Type", "application/json")
+            ResponseJSON(res, 404, ({
             error: "not found", 
             message: "The request route does not exist!"
         }))
     }
+
+    // //using .pathname to set dynamic /api or /api with query parameters
+    // if(urlObj.pathname === '/api' && req.method === 'GET'){
+
+    //     let filteredDestination = queryParams(queryObj, destinations)
+
+    //     //fetching data based on query parameters
+    //     ResponseJSON(res, 200, filteredDestination)
+        
+    // }else if(req.url.startsWith('/api/continent') && req.method === 'GET'){
+
+    //     //extracting the last query param on url
+    //     const continent = req.url.split('/').pop()   
+        
+    //     //filtering data based on its locationType 
+    //     const filteredData = filtereddData(destinations, "continent", continent)
+       
+    //     ResponseJSON(res, 200, filteredData)
+
+    // }else if(req.url.startsWith('/api/country') && req.method === 'GET'){
+
+    //     //extracting the last query param on url
+    //     const country = req.url.split('/').pop()
+
+    //     //filtering data based on its locationType 
+    //     const filteredData = filtereddData(destinations, "country",  country)
+
+    //     ResponseJSON(res, 200, filteredData)
+    // }else{
+
+        
+    // }
 })
 
 //starting the server 
