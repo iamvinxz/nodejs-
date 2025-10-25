8ea044f6ebdb0ceb3f5e37f97438507f7492ac26
iamvinxz <john.khervin13@gmail.com>
2025-10-25 12:40:55 +0800

    comments

--- index.js
+++ index.js
@@ -11,15 +11,16 @@ const server = http.createServer( async (req,res) => {
 
     const destinations = await getDataFromDb()
 
+    //capturing query parameters 
     const urlObj = new URL(req.url, `http://${req.headers.host}`)
-
     const queryObj = Object.fromEntries(urlObj.searchParams)
 
+    //using .pathname to set dynamic /api or /api with query parameters
     if(urlObj.pathname === '/api' && req.method === 'GET'){
 
         let filteredDestination = queryParams(queryObj, destinations)
 
-        //fetching all data set
+        //fetching data based on query parameters
         ResponseJSON(res, 200, filteredDestination)
         
     }else if(req.url.startsWith('/api/continent') && req.method === 'GET'){
@@ -48,11 +49,11 @@ const server = http.createServer( async (req,res) => {
         ResponseJSON(res, 404, ({
             error: "not found", 
             message: "The request route does not exist!"
-        })
-    )
+        }))
     }
 })
 
+//starting the server 
 server.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
 }) 
