1260ce8b3d2e14a133f51562ba9f30a527e9fe06
iamvinxz <john.khervin13@gmail.com>
2025-10-25 12:26:10 +0800

    query parameter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import http from 'node:http'
 import { getDataFromDb } from './db.js'
 import { ResponseJSON } from './utils/schema.js'
 import { filtereddData } from './utils/filteredData.js'
+import { URL } from 'node:url'
+import { queryParams } from './utils/queryParams.js'
  
 const PORT = process.env.PORT | 8080
 
@@ -9,21 +11,33 @@ const server = http.createServer( async (req,res) => {
 
     const destinations = await getDataFromDb()
 
-    if(req.url === '/api' && req.method === 'GET'){
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
 
-        ResponseJSON(res, 200, destinations)
+    const queryObj = Object.fromEntries(urlObj.searchParams)
+
+    if(urlObj.pathname === '/api' && req.method === 'GET'){
+
+        let filteredDestination = queryParams(queryObj, destinations)
+
+        //fetching all data set
+        ResponseJSON(res, 200, filteredDestination)
         
     }else if(req.url.startsWith('/api/continent') && req.method === 'GET'){
 
         //extracting the last query param on url
-        const continent = req.url.split('/').pop()           
+        const continent = req.url.split('/').pop()   
+        
+        //filtering data based on its locationType 
         const filteredData = filtereddData(destinations, "continent", continent)
        
         ResponseJSON(res, 200, filteredData)
 
     }else if(req.url.startsWith('/api/country') && req.method === 'GET'){
 
+        //extracting the last query param on url
         const country = req.url.split('/').pop()
+
+        //filtering data based on its locationType 
         const filteredData = filtereddData(destinations, "country",  country)
 
         ResponseJSON(res, 200, filteredData)
