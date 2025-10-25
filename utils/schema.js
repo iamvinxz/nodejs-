0edf8ece7a6b543622f9a8cf80b856437799ca30
iamvinxz <john.khervin13@gmail.com>
2025-10-25 13:07:19 +0800

    CORS

--- utils/schema.js
+++ utils/schema.js
@@ -1,5 +1,7 @@
 export const ResponseJSON = (res, status, data) => {
     res.statusCode = status
     res.setHeader("Content-Type","application/json")
+    res.setHeader("Access-Control-Allow-Origin", "*")           //setting up CORS so it can be access from any other domain or ports as long as they are using GET method 
+    res.setHeader("Access-Control-Allow-Methods", 'GET')        
     res.end(JSON.stringify(data)) 
 }
